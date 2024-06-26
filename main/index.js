14367b601f68a1c1a96a2bf9aad50fdef2911f01
尹成诺 <yinchengnuo@qq.com>
2024-06-26 08:35:15 +0800

    .

--- main/index.js
+++ main/index.js
@@ -4,7 +4,7 @@ import { app, BrowserWindow, ipcMain, dialog, Menu, globalShortcut, powerSaveBlo
 
 let updater
 const ENV = 'local'
-Menu.setApplicationMenu(null)
+// Menu.setApplicationMenu(null)
 const { autoUpdater } = electronUpdater
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const URL = envs.find((e) => e.tag === ENV).url
@@ -67,10 +67,10 @@ if (gotTheLock) {
                 select('')
             }
         })
-        window.on('blur', () => globalShortcut.unregisterAll())
-        window.on('focus', () => globalShortcut.register('CommandOrControl+R', () => window.webContents.reload()))
+        // window.on('blur', () => globalShortcut.unregisterAll())
+        // window.on('focus', () => globalShortcut.register('CommandOrControl+R', () => window.webContents.reload()))
     })
-    app.on('will-quit', () => globalShortcut.unregisterAll())
+    // app.on('will-quit', () => globalShortcut.unregisterAll())
     app.on('window-all-closed', () => app.quit())
 } else {
     app.quit()
